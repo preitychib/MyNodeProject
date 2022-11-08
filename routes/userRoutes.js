93661d6f75112a4c17dbe9b9fac98aee71bd5b10
preitychib <preeti.chib2015@gmail.com>
2022-11-08 16:23:37 +0530

    Added api.yaml

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,8 @@ const {
 } = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
-router.post("/", registerUser);
 router.post("/login", loginUser);
+router.post("/", registerUser);
 router.get("/me", protect, getMe);
 
 module.exports = router;
